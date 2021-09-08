12b42394a7ca7a2caf89f8d23e6f091c898b9d68
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2021-09-08 21:31:48 +0900

    Add files via upload

---
+++ 名称未設定フォルダ/sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void){
+    int num;
+    int array[num];
+    int ans;
+    int i;
+    printf("配列の要素数は？");
+    scanf("%d", &num);
+    printf("小さい方から入力して下さい\n");
+    printf("1番目：");
+    scanf("%d", &array[0]);
+}
