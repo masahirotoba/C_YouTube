b7b8e62cf7f1a69f6d77ebb621441aa384a0bbb2
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2021-09-10 15:35:32 +0900

    番兵を使った線形探索

---
+++ 探索アルゴリズム/#2_Tansaku.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int search(int array2[], int num2, int ans2){
+    int i = 0;
+    array2[num2] = ans2;
+    while(1){
+        if(array2[i] == ans2)
+            break;
+        i++;
+    }
+    if(i == num2)
+        return -1;
+    else return i;
+}
+
+int main (void){
+    int num;
+    int ans;
+    int array[num+1];
+    int i;
+    printf("配列の要素数は？");
+    scanf("%d", &num);
+    for(i = 0; i < num; i++){
+        printf("%d番目：", i + 1);
+        scanf("%d", &array[i]);
+    }
+    printf("探す数は？");
+    scanf("%d", &ans);
+    int result = search(array, num, ans);
+    if(result == -1)
+        printf("失敗です");
+    else printf("%dは%d番目にありました", ans, result + 1);
+    return 0;
+} 
