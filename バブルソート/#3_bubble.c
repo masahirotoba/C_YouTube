24fb72a6596573089cef2a26e0b85d1954a8877e
Masahiro Toba <86113516+masahirotoba@users.noreply.github.com>
2021-10-14 08:32:57 +0900

    終了条件を追加

---
+++ バブルソート/#3_bubble.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void swap (int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble (int y[], int num3){
+    int i, j;
+    int koukan = 0;
+    for(i = 0; i < num3 - 1; i++){
+        for(j = num3 - 1; j > i; j--){
+            if(y[j-1] > y[j])
+                swap(&y[j], &y[j-1]);
+        }
+        if (koukan == 0)
+            break;
+    }
+}
+
+void show (int x[], int num2){
+    int i;
+    for(i = 0; i < num2; i++){
+        printf("%d, ", x[i]);
+    }
+    printf("\n");
+}
+
+int main (void){
+    int num;
+    int array[num];
+    int i;
+    printf("要素数を入力して下さい\n");
+    scanf("%d", &num);
+    for(i = 0; i < num; i++){
+        scanf("%d", &array[i]);
+    }
+    printf("■１回目の出力\n");
+    show(array, num);
+    printf("■バブルソート後\n");
+    bubble(array, num);
+    show(array, num);
+    return 0;
+}
